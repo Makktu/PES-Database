c957bcd026208dc0bbbbaf7b461d4ad57ef9e6cf
Makktu <johnmcna@gmail.com>
2021-12-14 19:10:32 +0000

    now

--- script.js
+++ script.js
@@ -2,24 +2,30 @@
 
 // import * as data from 'data.json';
 
-const form = document.getElementById("form");
-const search = document.getElementById("search");
+// const form = document.getElementById("form");
+// const search = document.getElementById("search");
 
-const IMG_PATH = "/img/boxart/";
+// const IMG_PATH = "/img/boxart/";
 
-const games = require("data.json");
+// const games = require("data.json");
 
-const mainPart = document.querySelector("main");
+// const mainPart = document.querySelector("main");
 
-const gameEl = document.createElement("div");
+// const gameEl = document.createElement("div");
 
-const testCase = IMG_PATH + "Goal_Storm_PAL_version_1996.jpg";
+// const testCase = IMG_PATH + "Goal_Storm_PAL_version_1996.jpg";
 
-console.log(testCase);
+// console.log(testCase);
 
-gameEl.innerHTML = `<p>HELLO WORLD?</p><img src="${testCase}">`;
+// gameEl.innerHTML = `<p>HELLO WORLD?</p><img src="${testCase}">`;
 
 // access JSON of all games and display them on a single page
 // games.forEach((game) => {
 //     // code to do it goes here
 // });
+
+const el = document.querySelector(".img");
+el.src = "img/boxart/Goal_Storm_PAL_version_1996.jpg";
+el.addEventListener("load", () => {
+    el.classList.add("fadeIn");
+});
